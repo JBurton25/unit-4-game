347efac4491bc46e1329111d4c2cc3fc1940b75b
JBurton25 <jordonalexander@gmail.com>
2018-07-24 09:36:34 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,49 @@
 
-var wins;
-var losses;
-var counter
+var targetNumber= "";
+var wins= 0 ;
+var losses= 0;
+var counter= 0;
+var images =["assets/images/bluecystal.png", "assets/images/greencrystal.png", "assets/images/redcrystal.png", "assets/images/whitecrystal.png"];
 
-for (i = 0; i < 4; i++) {
-    var randomNumber = Math.floor(Math.random() * 12) + 1;
-    $("#crystalValue").append(randomNumber[i]);  
+function randomtargetNumber() {
+  var targetNumber = Math.floor(Math.random() * 120) + 19;
+$(".targetNumber").text("Number to hit: " + targetNumber);
 }
 
-var targetNumber = Math.floor(Math.random() * 120) +19;
+randomtargetNumber();
 
-$(".targetNumber").text("Number to hit: " + targetNumber);
+function setCrystals() {
+    for (var i = 0; i < images.length; i++) {
+        var crystalimage = $("<img>");
+        crystalimage.addClass("crystal-image");
+        crystalimage.attr("src", images[i]);
+        crystalimage.attr("value", (Math.floor(Math.random() * 12) +1));
+        crystalimage.attr("height", "100");
+        $("#crystals").append(crystalimage);
+
+    }
+}
+
+setCrystals();
+
+function reset() {
+    randomtargetNumber();
+    setCrystals();
+    counter = 0
+}
+
+$(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("value"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+    
+    $(".userNumber").text("Score: " + counter);
+
+    if (counter == targetNumber) {
+        wins ++;
+    } else if (counter > targetNumber) {
+        losses++;
+    };
+
+});
+
